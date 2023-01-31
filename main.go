49e4ab4328e2b87e839eb5172e70882b552de4cc
harig8431 <harigovindg8431@gmail.com>
2023-01-31 14:23:02 +0530

    initialted functions

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+func main()  {
+	
+	ns:=GetNutritionalScore(NutritionalData{
+		Energy:EnergyFromKcal(),
+		Sugars:SugarGram(),
+		SatturatedFattyAcids:SatturatedFattyAcidsGram(),
+		Sodium:Sodiummilligram(),
+		Fruits:FruitsPercent(),
+		Fibre:Fibregram(),
+		Protein:ProteinGram(),
+	},Food)
+
+	fmt.Println("Nutritional Score: %d",ns.Value)
+}
